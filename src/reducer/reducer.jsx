5772e843e5871c8538db0d35f6145e0940e197c5
Shubham Shekhar <shubhamshekhar123.ss@gmail.com>
2020-04-18 20:33:57 +0530

    added session

--- src/reducer/reducer.jsx
+++ src/reducer/reducer.jsx
@@ -3,9 +3,16 @@ const iState = {
     login:false,
     email:""
 }
+if(sessionStorage.getItem('forum_name')){
+    iState.name=sessionStorage.getItem('forum_name')
+    iState.email=sessionStorage.getItem('forum_email')
+    iState.login=true
+}
 
 const reducer =(state=iState,action)=>{
     if(action.type==='LOGOUT'){
+        sessionStorage.removeItem('forum_name')
+        sessionStorage.removeItem('forum_email')
         return {
             name:"",
             login:false,
@@ -13,6 +20,9 @@ const reducer =(state=iState,action)=>{
         }
     }
     if(action.type==='LOGIN'){
+        sessionStorage.setItem('forum_name',action.username)
+        sessionStorage.setItem('forum_email',action.useremail)
+        console.log(sessionStorage.getItem('forum_name'),sessionStorage.getItem('forum_email'))
         return {
             name:action.username,
             login:true,
